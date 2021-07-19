e4753cc688f7b17d04d19887c6038652ea480951
Oleg Andreyev <oleg@andreyev.lv>
2021-07-20 01:18:38 +0300

    added github actions

--- tests/Basic/BasicAuthTest.php
+++ tests/Basic/BasicAuthTest.php
@@ -22,7 +22,6 @@ class BasicAuthTest extends TestCase
         $this->assertStringContainsString($pageText, $session->getPage()->getContent());
     }
 
-    /** @psalm-return \Generator<int, array{0: string, 1: string, 2: string}, mixed, void> */
     public function setBasicAuthDataProvider(): \Generator
     {
         yield ['mink-user', 'mink-password', 'is authenticated'];
