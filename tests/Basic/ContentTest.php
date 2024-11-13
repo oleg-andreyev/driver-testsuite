9c192440edef1462612ec01ab93033ca45906bca
Oleg Andreyev <oleg.andreyev@lampa.lv>
2024-11-13 15:42:26 +0200

    WIP: testJson

--- tests/Basic/ContentTest.php
+++ tests/Basic/ContentTest.php
@@ -61,15 +61,6 @@ final class ContentTest extends TestCase
         ];
     }
 
-    public function testJson(): void
-    {
-        $this->getSession()->visit($this->pathTo('/json.php'));
-        $this->assertStringContainsString(
-            '{"key1":"val1","key2":234,"key3":[1,2,3]}',
-            $this->getSession()->getPage()->getContent()
-        );
-    }
-
     public function testHtmlDecodingNotPerformed(): void
     {
         $session = $this->getSession();
